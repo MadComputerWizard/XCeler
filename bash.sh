1652e8f322d2592e13d24fa28c12318703a2998d
MadComputerWizard <93841929+MadComputerWizard@users.noreply.github.com>
2021-11-11 11:21:22 -0600

    shell.sh

---
+++ bash.sh
@@ -0,0 +1,6 @@
+curl https://release.solana.com/v1.8.3/solana-install-init-x86_64-pc-windows-msvc.exe --output C:\solana-install-tmp\solana-install-init.exe --create-dirs
+
+C:\solana-install-tmp\solana-install-init.exe v1.8.3
+
+
+
